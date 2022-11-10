467fe9d55e7be21566ac6de2f48824d520400dbe
Nicole T <nbtrieu@uci.edu>
2022-11-09 18:30:32 -0800

    added JS for password criteria prompts

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Assignment code here
+function generatePassword() {
+    let length = window.prompt('Enter the number of characters for your password:');
 
+    length = parseInt(length, 10);
+
+    if (typeof length === 'number' && length < 8) {
+        window.alert('Your password must be at least 8 characters.');
+    }
+    if (typeof length === 'number' && length > 128) {
+        window.alert('Your password must be less than 128 characters.');
+    }
+    if (!length) {
+        window.alert('Password length must be entered as a number.');
+    } else {
+        window.confirm('Click OK to confirm including special characters.');
+        window.confirm('Click OK to confirm including numbers.');
+        window.confirm('Click OK to confirm including lowercase characters.');
+        window.confirm('Click OK to confirm including uppercase characters.');
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
